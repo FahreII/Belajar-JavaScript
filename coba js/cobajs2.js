1472f81045b22b95499b2d9ed04effc333c55340
Abitien Ahmad Fahrel <abitienfahrel@gmail.com>
2025-10-29 21:55:22 +0700

    update

--- coba js/cobajs2.js
+++ coba js/cobajs2.js
@@ -1,6 +1,15 @@
-function sapa() {
+/* function nama() {
     let nama = "Fahrel";
     console.log("halo " + nama + " selamat datang di javascript");
+    
+    
+sapa()
+}  */
+
+
+function sapa() {
+    let warna = "Hijau";
+    console.log("mobil baru saya berwarna " + warna);
 }
 
 sapa()
